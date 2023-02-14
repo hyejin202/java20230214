30d125e4f79791e8e74ab3cf1bba6068e9abaa73
hyejin202 <123609520+hyejin202@users.noreply.github.com>
2023-02-14 10:54:33 +0900

    first

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,25 @@
+package ch01.sec11;
+
+/**
+ * @author admin 여러분의 이름
+ * 
+ */
+/*
+  장제목 : 1장 자바 시작하기
+  작성일: 2022.08.24
+ */
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+	// 한 줄 주석
+	/*
+	 * 여러줄 주석
+	 * 여러줄 주석
+	 * 여러줄 주석
+	 */
+	//단축키 :ctrl + /
+		
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+}
---
+++ src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,18 @@
+package ch01.sec11;
+
+public class MainShortCut {
+	public static void main(String[] args) {
+		//sysout
+		System.out.println("Hi JAVA");
+		System.out.println("출력되는 내용");
+		System.out.println("출력!!");
+		
+		// 주석 ctrl + /
+		//실행 ctrl + f11
+		//창크기 ctrl + m
+		//한줄 지우기 ctrl + d
+		// 줄 옮기기 alt + 방향키 위아래
+		// 힌트얻기 ctrl + space
+		
+	}
+}
