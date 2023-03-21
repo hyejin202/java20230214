b4f99f635b087dfbb10957699dbc8cb6b1eb07e1
hyejin202 <123609520+hyejin202@users.noreply.github.com>
2023-03-21 17:50:40 +0900

    ..

--- src/test/T01test01.java
+++ src/test/T01test01.java
@@ -73,12 +73,19 @@ public class T01test01 {
 		}
 		
 		System.out.println();
+		
 		System.out.println("6");
-//			 *  
+//		  	 *  
 //		    ***
 //		   *****
 //		  *******
 //		 *********
+		for(int i=0; i<5; i++) {
+			for(int j=1; j<(2*j)+1; j+=2) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 		
 		
 		System.out.println("7");
