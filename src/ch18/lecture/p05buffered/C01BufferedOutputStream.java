fe5759d09d0a41b57dda820bb5f3a2fdf8518e54
hyejin202 <123609520+hyejin202@users.noreply.github.com>
2023-03-22 16:39:32 +0900

    03-22

---
+++ src/ch18/lecture/p05buffered/C01BufferedOutputStream.java
@@ -0,0 +1,30 @@
+package ch18.lecture.p05buffered;
+
+import java.io.*;
+
+public class C01BufferedOutputStream {
+	public static void main(String[] args) {
+		String name = "output/buffered1.txt";
+		try {
+			OutputStream fis = new FileOutputStream(name);
+
+			OutputStream os = new BufferedOutputStream(fis);
+//			BufferedOutputStream os = new BufferedOutputStream();
+
+			long start = System.nanoTime();
+
+			for (int i = 0; i < 1000_000; i++) {
+				os.write(1);
+			}
+			os.flush();
+
+			long end = System.nanoTime();
+
+			System.out.println((end - start) + "ns");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
---
+++ src/ch18/lecture/p05buffered/C03BufferedInputStream.java
@@ -0,0 +1,23 @@
+package ch18.lecture.p05buffered;
+
+import java.io.*;
+
+public class C03BufferedInputStream {
+	public static void main(String[] args) {
+		String name = "output/buffered1.txt";
+		
+		try(InputStream fis = new FileInputStream(name)) {
+			InputStream is = new BufferedInputStream(fis);
+			
+			long start = System.nanoTime();
+			
+			while(is.read() != -1) {
+				
+			}
+			long end  = System.nanoTime();
+			System.out.println((end - start) + "ns");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
