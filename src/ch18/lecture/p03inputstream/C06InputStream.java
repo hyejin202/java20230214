474f815ca4f09352fbcfe4b43ea8fd344c219d07
hyejin202 <123609520+hyejin202@users.noreply.github.com>
2023-03-22 16:08:49 +0900

    03-22

---
+++ src/ch18/lecture/p03inputstream/C06InputStream.java
@@ -0,0 +1,27 @@
+package ch18.lecture.p03inputstream;
+
+import java.io.*;
+
+public class C06InputStream {
+	public static void main(String[] args) {
+		String src = "output/bird.jfif";
+		String des = "output/bird_copy2.jfif";
+		
+		try (var is = new FileInputStream(src);
+				var os = new FileOutputStream(des);){
+			byte[] data = new byte[1024];
+			
+			int len = 0;
+			while ((len = is.read(data)) != -1) {
+				os.write(data, 0, len);
+			}
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("복사완료");
+		
+		
+	}
+}
